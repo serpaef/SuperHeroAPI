e48e15b9707a8a83a1128fd8e4f63ac9fe72b94f
Fernando Serpa <serpaef@outlook.com>
2022-10-05 10:35:42 -0300

    feat: Set connection string and config db on main app

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -1,3 +1,6 @@
+global using SuperHeroAPI.Data;
+global using Microsoft.EntityFrameworkCore;
+
 namespace SuperHeroAPI
 {
     public class Program
@@ -9,6 +12,10 @@ namespace SuperHeroAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
